50fa0ae4e63b8b2b579eab490c1d021e1ddf0428
chumex412 <64347090+chumex412@users.noreply.github.com>
2021-12-03 16:54:58 +0100

    Added  and imported  loader

---
+++ src/components/Loader.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Loader = () => {
+
+  return (
+    <span className="loader-wrapper">
+      <span className="loader">
+        <span>L</span>
+        <span>O</span>
+        <span>A</span>
+        <span>D</span>
+        <span>I</span>
+        <span>N</span>
+        <span>G</span>
+        <span>.</span>
+        <span>.</span>
+        <span>.</span>
+      </span>
+    </span>
+  )
+};
+
+export default Loader;
