76943e03b7e63b7e9fe9936c9cd8d3cbf69f2a21
chumex412 <64347090+chumex412@users.noreply.github.com>
2021-12-03 15:42:36 +0100

    Added data for exchange

---
+++ src/components/data.js
@@ -0,0 +1,41 @@
+
+const exchangeData = [
+  {
+    category: "cryptocurrency",
+    name: "Bitcoin (BTC)",
+    code: "BTC",
+    id: "1"
+  },
+  {
+    category: "cryptocurrency",
+    name: "Ethereum (ETH)",
+    code: "ETH",
+    id: "1027"
+  },
+  {
+    category: "cryptocurrency",
+    name: "Binance Coin (BNB)",
+    code: "BNB",
+    id: "1839"
+  },
+  {
+    category: "currency",
+    name: "United States Dollar \"$\" (USD)",
+    code: "USD",
+    id: "2781"
+  },
+  {
+    category: "currency",
+    name: "Euro \"€\" (EUR)",
+    code: "EUR",
+    id: "2790"
+  },
+  {
+    category: "currency",
+    name: "Ugandan Shilling \"Sh\" (UGX)",
+    code: "UGX",
+    id: "3570"
+  }
+]
+
+export default exchangeData;
