36e390f2e47d42f07249801daf356549ed8391c7
chumex412 <64347090+chumex412@users.noreply.github.com>
2021-12-03 15:44:44 +0100

    imported necessary components and rendered data from API endpoint

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import RatesForm from './components/RatesForm';
+import RatesConversion from './components/RateConversion';
+import exchangeData from './components/data';
 import './App.css';
 
+const baseURL = `https://api.coinmarketcap.com/data-api/v3/tools/price-conversion?amount=`;
+
 function App() {
+  const [amount, setAmount] = useState("1");
+  const [options, setOptions] = useState(exchangeData);
+  const [convertFrom, setConvertFrom] = useState(exchangeData[0]);
+  const [convertTo, setConvertTo] = useState(exchangeData[3]);
+  const [rateData, setRateData] = useState(null);
+  //const [price, setPrice] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchRate = async () => {
+      
+      setLoading(true);
+
+      try {
+        const response = await fetch(`${baseURL}${amount}&convert_id=${convertTo.id}&id=${convertFrom.id}`);
+        const resData = await response.json();
+
+        if(!resData) {
+          throw new Error("Rates data cannot be fetched");
+        }
+        
+        const {data} = resData;
+        setRateData(data);
+        //setPrice()
+        console.log(data)
+        setLoading(false)
+      } catch(err) {
+        console.log(err)
+        setLoading(false);
+        setError(err)
+      }
+    }
+
+    fetchRate();
+
+  }, [amount, convertTo, convertFrom]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <section className="conversion-content">
+          <RatesForm 
+            value={amount}
+            setValue={setAmount}
+          />
+
+          <RatesConversion
+            options={options}
+            convertTo={convertTo}
+            convertFrom={convertFrom}
+            setConvertTo={setConvertTo}
+            setConvertFrom={setConvertFrom}
+            amount={amount}
+            price={rateData?.quote[0]?.price}
+            loading={loading}
+          />
+        </section>
+      </div>
     </div>
   );
 }
